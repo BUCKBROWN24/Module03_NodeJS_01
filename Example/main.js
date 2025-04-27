047874c71ab378bd2b6e3c2595358bdb94a0ac22
BUCKBROWN24 <brownbruce64@gmail.com>
2025-04-27 14:56:30 -0500

    Create main.js

---
+++ Example/main.js
@@ -0,0 +1,15 @@
+
+      // Operations: Object Access, Invocation
+      console.log("What's your name?");
+
+      // Operations:
+      process.stdin.on('data', function(data) {
+
+          // Operations:
+          const name = data.toString().trim();
+          
+          // Operations:
+          console.log(`Hello, ${name}!`);
+
+      });
+      
